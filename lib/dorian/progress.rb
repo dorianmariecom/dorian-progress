f625c9edc54266309d7bc0db2377dfb967a75a6a
Dorian Marié <dorian@dorianmarie.com>
2024-08-24 23:15:18 +0200

    fix format default value in progress creation

--- lib/dorian/progress.rb
+++ lib/dorian/progress.rb
@@ -7,8 +7,8 @@ class Dorian
       minimal: nil
     }
 
-    def self.create(total: nil, format: :full, output: nil)
-      format = FORMATS.fetch(format.to_sym) if format
+    def self.create(total: nil, format: nil, output: nil)
+      format = FORMATS.fetch((format || :full).to_sym) if format
       ProgressBar.create(total:, format:, output:)
     end
   end
